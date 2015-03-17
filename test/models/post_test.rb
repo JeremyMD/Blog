940e44dd3394759789665938014968644e697cf1
James Kennedy <james@jameskennedy.ie>
2015-03-17 10:34:14 +0200

    Added tests to be fixed

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "A post should always have a title and a content" do
+    post = Post.new title: nil, content: "okay"
+
+    assert !post.save, "Every post should have a title"
+
+    post2 = Post.new title: "Valid", content: nil
+
+    assert !post.save, "Every post should have some content"
+
+  end
 end
