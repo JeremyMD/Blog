655805dee30d667b14fc36ab7a8615e7275cd963
James Kennedy <james@jameskennedy.ie>
2015-03-17 12:07:04 +0200

    Improved thie test

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -2,9 +2,13 @@ require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
   test "a comment should always have a post associated" do
-  	
+
   	@comment = Comment.new text_area: "Something", post_id: nil
   	assert !@comment.save, "I was able to create  a comment without having to specify a post"
+
+  	@comment = Comment.new text_area: "Content", post_id: 1
+
+  	assert @comment.save
 
   end
 
